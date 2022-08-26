8b4184fcd60b39761c093eec7e5dd9b2831b8e98
Samuel Rey <samuel.rey.escudero@gmail.com>
2022-08-27 01:33:27 +0200

    updating test efficient algorithm

--- python/opt.py
+++ python/opt.py
@@ -1,6 +1,8 @@
 import cvxpy as cp
 import numpy as np
 from scipy.linalg import khatri_rao
+from scipy.sparse import kronsum
+
 
 # Imported here to be able to call getattr(opt, func_name)
 from opt_sota import estH_llsscp
@@ -410,17 +412,135 @@ def rfi(X, Y, Sn, Cy, params, max_iters=20, th=1e-3):
     return H, S, diff_H, diff_S
 
 
-# def efficient_filter_id(h_prev):
-#     mu = 1
+def gradient_filter_id(XX, S, y_kron, h, gamma):
+    """
+    Computes the gradient of the objective funciton employed in the filter identification step.
+    """
+    N = XX.shape[0]
+
+    # Define vectorization for  kron(I, A)b  operation and block-diag times a vector
+    diag_blocks_vec = lambda A, b: np.array([A[:,i].repeat(N)*np.tile(b[i*N:(i+1)*N], N) for i in range(N)]).sum(axis=0)
+    block_diag_vec = lambda A, b: np.concatenate([A@b[i*N:(i+1)*N] for i in range(N)])
+
+    zh = block_diag_vec(S, h) - diag_blocks_vec(S, h)
+    zzh = block_diag_vec(S, zh) - diag_blocks_vec(S, zh)
+    return diag_blocks_vec(XX, h) - y_kron + gamma*zzh
 
 
+def efficient_filter_id(XX, S, y_kron, gamma, mu, H_init=None, max_iters=100, eps=1e-3, verbose=False):
+    """
+    Solves the filter identification step following a gradient descent algorithm
+    """
+    h_iters = np.zeros((max_iters, y_kron.shape[0]))
+    h = H_init.flatten('F') if H_init is not None else np.zeros(y_kron.shape[0])
+
+    for i in range(max_iters):
+        h = h - mu*gradient_filter_id(XX, S, y_kron, h, gamma)
+        h_iters[i,:] = h
+
+        if verbose:
+            print('Iter', i, ': norm(h):', np.linalg.norm(h))
+
+        # Stopping condition    
+        if i > 0:
+            if np.linalg.norm(h_iters[i,:] - h_iters[i-1,:])/np.linalg.norm(h_iters[i-1,:]) < eps:
+                break
+
+    return h.reshape(S.shape, order='F'), h_iters
 
 
-# def rfi_pgd(X, Y, Sn):
-#     N = X.shape[0]
+def sparse_graph_update(Z_T, s, sn, r, lambd, beta, gamma):
+    for i, (s_i, sn_i, z_i) in enumerate(zip(s, sn, Z_T)): 
+        r_i = np.asarray(r - z_i*s_i).squeeze() if s_i != 0 else r
+        z_times_r = (z_i@r_i)[0]
+        z_times_z = z_i.power(2).sum()
 
-#     H = efficient_filter_id()
+        ## get s[i] with projected soft-thresholding
+        if (-lambd - beta - gamma*z_times_r)/(gamma*z_times_z) > sn_i:
+            s_i = (-lambd - beta - gamma*z_times_r)/(gamma*z_times_z)
 
+        elif (lambd - beta - gamma*z_times_r)/(gamma*z_times_z) < sn_i:
+            s_i = (lambd - beta - gamma*z_times_r)/(gamma*z_times_z)
+
+        else:
+            s_i = sn_i
+
+        s[i] = s_i if s_i > 0 else 0
+        r = np.asarray(r_i + z_i*s_i).squeeze() if s_i != 0 else r_i
+
+    return s, r
+
+
+def efficient_graph_id(Sn, H, lambd, beta, gamma, S_init=None, max_iters=50, eps=5e-3, verbose=False):
+    """
+    Solves the graph denosing algorithm following a cyclic coordinate descent
+    """
+    N = Sn.shape[0]
+    low_idxs = np.concatenate([np.arange(i+1, N)+N*i for i in range(N)])
+    up_idxs = np.concatenate([np.arange((i+1)*N, N*(N-1)+1, N)+i for i in range(0,N-1)])
+    sn = Sn.flatten('F')[low_idxs]
+
+    # Assuming the graph is undirected
+    Z_T_whole = kronsum(-H, H, format='csr')
+    Z_T = Z_T_whole[low_idxs,:] + Z_T_whole[up_idxs,:]
+    if S_init is not None:
+        s = S_init.flatten('F')[low_idxs]
+        r = s@Z_T
+    else:
+        s = np.zeros(sn.size)
+        r = np.zeros(Z_T.shape[1])
+
+    s_iters = np.zeros((max_iters, sn.size))
+    for i in range(max_iters):
+        s, r = sparse_graph_update(Z_T, s, sn, r, lambd, beta, gamma)
+        s_iters[i,:] = s
+
+        if verbose:
+            print('Iter', i, ': norm(s):', np.linalg.norm(s))
+
+        # Stopping condition    
+        if i > 0:
+            if np.linalg.norm(s_iters[i,:] - s_iters[i-1,:])/np.linalg.norm(s_iters[i-1,:]) < eps:
+                break
+
+    # reconstruction from triangular matrix
+    S_flat = np.zeros(H.shape).flatten('F')
+    mask_range = np.arange(N)
+    mask_lower = mask_range[:,None] > mask_range
+    S_flat[mask_lower.flatten('F')] = s
+    S = S_flat.reshape((N,N), order='F')
+    S = S + S.T
+    return S, s_iters
+
+
+def efficient_rfi(X, Y, Sn, regs, iters_out=20, iters_filter=10, iters_graph=10):
+    # Initialization
+    lamb, gamma, beta, inc_gamma, mu = regs
+    N = X.shape[0]
+
+    # Precomputing quantities
+    y = Y.flatten()
+    y_kron = np.kron(X, np.eye(N))@y
+    XX = X@X.T
+    
+    # Init S and H to 0
+    Ss_hat = np.zeros((iters_out+1, N, N))
+    Ss_hat[0] = Sn
+    Hs_hat = np.zeros((iters_out+1, N, N))
+    for i in range(1, iters_out+1):
+        Hs_hat[i], _ = efficient_filter_id(XX, Ss_hat[i-1], y_kron, gamma, mu,
+                                           H_init=Hs_hat[i-1], max_iters=iters_filter)
+        Ss_hat[i], _ = efficient_graph_id(Sn, Hs_hat[i], lamb, beta, gamma, S_init=Ss_hat[i-1],
+                                          max_iters=iters_graph)
+        
+        gamma *= inc_gamma
+
+        # Stopping condition
+
+    return Hs_hat[i], Ss_hat[i], Hs_hat, Ss_hat
+
+
+# Baselines
 def estH_tls_sem(X, Y, Sn, Cy, params, max_iters=20, th=1e-3, patience=4, H_true=None, S_true=None, verb=VERB):
     N = Sn.shape[0]
     lambd1, lambd2 = params
